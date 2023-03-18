122c407c614cdc4e2d4fbf86e48d5fcd09e53b54
phantombass <tmkeaton@ngu.edu>
2023-03-18 06:39:16 -0400

    1.0.0

--- Plugins/HegeMEANY Custom Scripts/00_Settings.rb
+++ Plugins/HegeMEANY Custom Scripts/00_Settings.rb
@@ -45,7 +45,6 @@ module Game
     SaveData.load_new_game_values
     $stats.play_sessions += 1
     $close_dexnav = 0
-
     $map_log = MapLog.new
     $map_factory = PokemonMapFactory.new($data_system.start_map_id)
     $game_player.moveto($data_system.start_x, $data_system.start_y)
