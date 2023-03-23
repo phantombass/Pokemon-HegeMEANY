8412e448ffa60dff46310464e0cf4ea1c3cc5e13
phantombass <tmkeaton@ngu.edu>
2023-03-23 17:29:20 -0400

    1.0.14

--- Plugins/Phantombass AI v20.1/01_AI_Main.rb
+++ Plugins/Phantombass AI v20.1/01_AI_Main.rb
@@ -847,6 +847,7 @@ class PBAI
       $d_switch = 1 if $doubles_switch != nil
       $target_strong_moves = false
       switch = ai_should_switch?
+      switch = false if !self.can_switch?
       # Get the optimal switch choice by type
       scores = get_optimal_switch_choice
       # If we should switch due to effects in battle
